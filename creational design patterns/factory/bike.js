c8ac6bbf59a56c2ee23f7bb813978aa89b75d6ce
raghvendra <rghvndr99@gmail.com>
2025-04-06 09:49:29 +0530

    updating read me

---
+++ creational design patterns/factory/bike.js
@@ -0,0 +1,8 @@
+export default class Bike {
+    constructor(name){
+        this.name=name;
+    }
+     print() {
+        console.log(`your bike is of ${this.name} brand`);
+     }
+}
---
+++ creational design patterns/factory/truck.js
@@ -0,0 +1,8 @@
+export default class Truck {
+    constructor(name){
+        this.name=name;
+    }
+     display() {
+        console.log(` ${this.name} is of heavy vehicle type`);
+     }
+}
